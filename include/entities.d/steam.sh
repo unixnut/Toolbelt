be90812e502c4aed4a53f6b3452e4585273dcfc5
Alastair Irvine <alastair@plug.org.au>
2022-01-04 00:09:50 +0800

    Added include/entities.d/steam.sh and package_keys/steam.gpg

---
+++ include/entities.d/steam.sh
@@ -0,0 +1,14 @@
+commands[steam]="add_repository_apt -k steam.gpg \
+                                       steam https://repo.steampowered.com/steam/ steam stable
+  $APT install $APT_OPTIONS steam-launcher"
+
+straplines[steam]="Launcher for the Steam software distribution service"
+descriptions[steam]="Steam is a software distribution service with an online store, automated
+installation, automatic updates, achievements, SteamCloud synchronized
+savegame and screenshot functionality, and many social features.
+
+http://www.steampowered.com/"
+## dependencies[steam]=
+## messages[steam]=""
+
+## aliases[steam]=

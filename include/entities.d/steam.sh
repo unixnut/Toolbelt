7fe954d4c6f73a7e52ef6dd97b21ce22e9c50c23
Alastair Irvine <alastair@plug.org.au>
2022-01-07 00:00:20 +0800

    include/entities.d/steam.sh: Now installs 'steam' package

--- include/entities.d/steam.sh
+++ include/entities.d/steam.sh
@@ -1,11 +1,15 @@
 commands[steam]="add_repository_apt -k steam.gpg \
                                        steam https://repo.steampowered.com/steam/ steam stable
-  $APT install $APT_OPTIONS steam-launcher"
+  $APT install $APT_OPTIONS -o Dpkg::Options::=--force-confnew steam"
 
 straplines[steam]="Launcher for the Steam software distribution service"
 descriptions[steam]="Steam is a software distribution service with an online store, automated
 installation, automatic updates, achievements, SteamCloud synchronized
 savegame and screenshot functionality, and many social features.
+
+(This Toolbelt entity downloads 'steam' from the Valve package archive.  It is
+an alternative to 'steam-installer' in the Ubuntu package archive and 
+'steam-launcher' (a.k.a steam.deb) from https://store.steampowered.com/ .) 
 
 http://www.steampowered.com/"
 ## dependencies[steam]=

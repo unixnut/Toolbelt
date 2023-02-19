26ebc6ce7af2e65c86d33c567be52530cf2efab9
Alastair Irvine <alastair@plug.org.au>
2023-02-19 18:09:29 +0800

    v1.3.1: Fix for AWS CLI v2: aborts installation if v1 detected
    toolbelt: Bumped version
    
    include/entities.d/aws-cli.sh:
      + Moved installation code into separate function install_aws_cli()
      + Detects if already installed from system package or via PIP and aborts

--- include/entities.d/aws-cli.sh
+++ include/entities.d/aws-cli.sh
@@ -1,18 +1,5 @@
 # Installs to /opt/aws/aws-cli-v2 using supplied installer
-commands[aws-cli]="install_dir=/opt/aws/aws-cli-v2
-  release_url=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
-  sig_url=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip.sig
-  fetch \$SCRIPT_TMPDIR \$release_url \$sig_url
-  pgp_verify \$SCRIPT_TMPDIR/\${release_url##*/} \$SCRIPT_TMPDIR/\${sig_url##*/}
-  echo '*** Extracting Archive:' \$SCRIPT_TMPDIR/\${release_url##*/} ...
-  unzip -q -d \$SCRIPT_TMPDIR/ \$SCRIPT_TMPDIR/\${release_url##*/}
-  if [ -d \$install_dir ] ; then
-    echo '*** Running AWS CLI installer in update mode'
-    \$SCRIPT_TMPDIR/aws/install -i \$install_dir --update
-  else
-    echo '*** Running AWS CLI installer'
-    \$SCRIPT_TMPDIR/aws/install -i \$install_dir
-  fi"
+commands[aws-cli]="install_aws_cli /opt/aws/aws-cli-v2"
 straplines[aws-cli]="AWS Command Line Interface"
 descriptions[aws-cli]="AWS CLI version 2
 
@@ -21,3 +8,34 @@ https://docs.aws.amazon.com/cli/index.html"
 ## messages[aws-cli]=""
 
 aliases[awscli]=aws-cli
+
+install_aws_cli()
+{
+  local install_dir=$1
+  local release_url=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
+  local sig_url=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip.sig
+
+  if [ -f /usr/bin/aws ] ; then
+    echo 'ERROR: AWS CLI v1 installed (from system package); uninstall before proceeding' >&2
+    return 2
+  fi
+
+  if [ -f /usr/local/bin/aws ] && [ ! -L /usr/local/bin/aws ] ; then
+    echo 'ERROR: AWS CLI v1 installed (via PIP); uninstall before proceeding' >&2
+    return 3
+  fi
+
+  fetch $SCRIPT_TMPDIR $release_url $sig_url
+  pgp_verify $SCRIPT_TMPDIR/${release_url##*/} $SCRIPT_TMPDIR/${sig_url##*/}
+
+  echo '*** Extracting Archive:' $SCRIPT_TMPDIR/${release_url##*/} ...
+  unzip -q -d $SCRIPT_TMPDIR/ $SCRIPT_TMPDIR/${release_url##*/}
+
+  if [ -d $install_dir ] ; then
+    echo '*** Running AWS CLI installer in update mode'
+    $SCRIPT_TMPDIR/aws/install -i $install_dir --update
+  else
+    echo '*** Running AWS CLI installer'
+    $SCRIPT_TMPDIR/aws/install -i $install_dir
+  fi
+}

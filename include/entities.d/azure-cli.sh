e7719ee4ac884c70bf8891d6d81231e463a0ebb3
Alastair Irvine <alastair@plug.org.au>
2025-08-27 16:13:07 +0800

    include/entities.d/azure-cli.sh: support for RedHat and derivatives
    Untested!

--- include/entities.d/azure-cli.sh
+++ include/entities.d/azure-cli.sh
@@ -1,6 +1,26 @@
-commands[azure-cli]="add_key_apt https://packages.microsoft.com/keys/microsoft.asc
+case $DISTRO_BASE in
+  Debian)
+    # Already available on Ubuntu on Azure thanks to http://azure.archive.ubuntu.com/ubuntu archive
+    commands[azure-cli]="add_key_apt https://packages.microsoft.com/keys/microsoft.asc
   add_repository_apt azure-cli https://packages.microsoft.com/repos/azure-cli/ main
   $APT install $APT_OPTIONS azure-cli"
+    ;;
+
+  RHEL)
+    case $DISTRO_RELEASE_MAJOR in
+      8)      commands[azure-cli]="add_repository_dnf -k https://packages.microsoft.com/keys/microsoft.asc \
+                                  microsoft-prod https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
+  dnf install azure-cli" ;;
+      9)      commands[azure-cli]="add_repository_dnf -k https://packages.microsoft.com/keys/microsoft.asc \
+                                  microsoft-prod https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm
+  dnf install azure-cli" ;;
+      1[0-9]) commands[azure-cli]="add_repository_dnf -k https://packages.microsoft.com/keys/microsoft-2025.asc \
+                                  microsoft-prod https://packages.microsoft.com/config/rhel/$DISTRO_RELEASE_MAJOR/packages-microsoft-prod.rpm
+        dnf install azure-cli" ;;
+    esac
+    ;;
+esac
+
 straplines[azure-cli]="Microsoft Azure command-line interface (Azure CLI)"
 descriptions[azure-cli]="Azure CLI is a cross-platform command-line tool for managing Azure
 resources with interactive commands or scripts.
